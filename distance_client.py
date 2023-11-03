1dc1dd55f5ce213078143d1c5f2295ec34baf324
pcruzn <pablo.cruz@gmail.com>
2023-11-02 21:52:40 -0300

    Minor improvements...

--- distance_client.py
+++ distance_client.py
@@ -4,25 +4,30 @@ import distance_unary_pb2 as pb2
 from google.protobuf.json_format import MessageToJson
 import json
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with grpc.insecure_channel("localhost:50051") as channel:
         stub = pb2_grpc.DistanceServiceStub(channel)
 
+        # a SourceDest contains two Position: source and destination
         message = pb2.SourceDest(
             source=pb2.Position(
-                latitude=-33.0351516, longitude=-71.5955963
+                latitude=-33.0351516, longitude=-71.5955963,
             ),
             destination=pb2.Position(
                 latitude=-33.0348327, longitude=-71.5980458
             ),
-            unit="km"
+            unit="nm"
         )
 
-        print(f"{MessageToJson(message)}\n")
+        print(f"Message sent:\n{MessageToJson(message)}\n")
 
         # call remote method
         response = stub.geodesic_distance(message)
 
-        print("Distance:", json.loads(MessageToJson(response))["distance"])
-        print("Method:", json.loads(MessageToJson(response))["method"])
-        print("Distance unit:", json.loads(MessageToJson(response))["unit"])
+        try:
+            print("-----Response-----")
+            print("Distance:", json.loads(MessageToJson(response))["distance"])
+            print("Method:", json.loads(MessageToJson(response))["method"])
+            print("Distance unit:", json.loads(MessageToJson(response))["unit"])
+        except KeyError:
+            print("One or more key are missing!")
