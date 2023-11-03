fdc2f962537470cf4f7e41625eac863f19ab48f9
pcruzn <pablo.cruz@gmail.com>
2023-11-02 21:52:20 -0300

    Default API operation, exceptions handling added

--- distance_grpc_service.py
+++ distance_grpc_service.py
@@ -9,22 +9,39 @@ from helpers import Distance
 class DistanceServicer(pb2_grpc.DistanceServiceServicer):
     def geodesic_distance(self, request, context):
         if request.unit == "km":
-            distance = Distance(
+            try:
+                distance = Distance(
                     Position(request.source.latitude, request.source.longitude, request.source.altitude),
                     Position(request.destination.latitude, request.destination.longitude, request.destination.altitude)
-            ).km()
-        if request.unit == "nm":
-            distance = Distance(
-                Position(request.source.latitude, request.source.longitude, request.source.altitude),
-                Position(request.destination.latitude, request.destination.longitude, request.destination.altitude)
-            ).nautical()
+                ).km()
+                response_map = {"distance": distance, "method": "geodesic", "unit": str(request.unit)}
+            except ValueError:
+                response_map = {"distance": float(-1), "method": "geodesic", "unit": "invalid"}
 
-        response_map = {"distance": distance, "method": "geodesic", "unit": str(request.unit)}
+        if request.unit == "nm":
+            try:
+                distance = Distance(
+                    Position(request.source.latitude, request.source.longitude, request.source.altitude),
+                    Position(request.destination.latitude, request.destination.longitude, request.destination.altitude)
+                ).nautical()
+                response_map = {"distance": distance, "method": "geodesic", "unit": str(request.unit)}
+            except ValueError:
+                response_map = {"distance": float(-1), "method": "geodesic", "unit": "invalid"}
+        # by default, if no unit is provided, distance is returned in unit = km
+        if request.unit == "":
+            try:
+                distance = Distance(
+                    Position(request.source.latitude, request.source.longitude, request.source.altitude),
+                    Position(request.destination.latitude, request.destination.longitude, request.destination.altitude)
+                ).km()
+                response_map = {"distance": distance, "method": "geodesic", "unit": "km"}
+            except ValueError:
+                response_map = {"distance": float(-1), "method": "geodesic", "unit": "invalid"}
 
         return pb2.Distance(**response_map)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     pb2_grpc.add_DistanceServiceServicer_to_server(DistanceServicer(), server)
     server.add_insecure_port("[::]:50051")
